f0dec41789bd192f5a759efd1632dd08b4cce923
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2024-01-06 13:56:48 -0800

    Read languages from the course repo

--- app/src/i18n/languages.ts
+++ app/src/i18n/languages.ts
@@ -1,12 +1,10 @@
-export const allLanguages = {
-	en: 'English',
-	de: 'Deutsch',
-} as const;
+import { courseLanguages } from "./translations/languages";
+export { rtlLanguages } from "./translations/languages";
+
+export const allLanguages = courseLanguages;
 
 /**
  * Map of language codes to a written out language name.
  * Used to populate the language switcher in the navbar.
  */
-export default allLanguages;
-
-export const rtlLanguages = new Set(['ar']);
+export default courseLanguages;
